3e0cfedc724650467b6269a039887e875fbb69b1
MohawkWarrior <jetstream2005.2005@gmail.com>
2025-11-10 17:36:34 +0100

    Update TestEtudiant.java

--- TestEtudiant.java
+++ TestEtudiant.java
@@ -1,1 +1,16 @@
+public class TestEtudiant {
+    public static void main(String[] args) {
 
+        Etudiant alice = new Etudiant("Alice Dupont");
+        alice.ajouterNote(new Notation(15.0, 3));
+        alice.ajouterNote(new Notation(8.0, 2));
+        alice.ajouterNote(new Notation(12.5, 4));
+        alice.afficher();
+
+
+        Etudiant bob = new Etudiant("Bob Martin");
+        bob.ajouterNote(new Notation(7.0, 5));
+        bob.ajouterNote(new Notation(11.0, 1));
+        bob.afficher();
+    }
+}
